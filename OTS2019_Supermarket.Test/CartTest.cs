d81d5208fea98531960f4e54eb1c1b87ed6c3eb8
it25-2024 <it25-2024@VP-06>
2026-04-15 14:41:24 +0200

    vezbe5

--- OTS2019_Supermarket.Test/CartTest.cs
+++ OTS2019_Supermarket.Test/CartTest.cs
@@ -8,8 +8,26 @@ using System.Threading.Tasks;
 
 namespace OTS_Supermarket.Test
 {
+    [TextFixture]
+
     public class CartTest
     {
+        [Test]
 
+        public void AddOneToCart_ShouldAddItemToCart_Success()
+        {
+            // arrange
+            Cart cart = new Cart();
+            Monitor monitor = new Monitor();
+
+            // act
+            card.AddOneToCart(monitor);
+
+            // assert
+            Assert.That(cart.Size, Is.EqualTo(1));
+            Assert.That(cart.Amount, Is.EqualTo(100));
+
+            // dozvoljene su nam 2 asertacije jer su promenjene JEDNOM metodom, INACE!! samo jedna asertacija!
+        }
     }
 }
